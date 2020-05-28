6841dd4fe65cf31c57fb017f26ebb599abd66541
lijiangwei <lijingweilijingwei@163.com>
2020-05-28 15:46:52 +0800

    添加代码

---
+++ base/assignment/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+ * 测试的常量
+ */
+const A1 = "xxx"
+
+func main() {
+	fmt.Println("hello !")
+	var a string
+	var b int
+	c := 1
+	fmt.Println("%v", a)
+	fmt.Println("%v", b)
+	fmt.Println("%v", c)
+	fmt.Println("%v", A1)
+}
