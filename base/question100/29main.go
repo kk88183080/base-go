b34a7982a4bf4e130792f64a3bf3ecb5a99c67de
jiangweili <441554737@qq.com>
2020-05-31 18:17:44 +0800

    添加29题答案

--- base/question100/29main.go
+++ base/question100/29main.go
@@ -1,5 +1,59 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	//_29testslice()
+
+	//_29testslice1()
+	//_29testslice2()
+	_29testslice3()
+}
+
+func _29testslice() {
+	v := []int{1, 2, 3}
+	for i, val := range v {
+		fmt.Println(i, val)
+		v = append(v, i)
+	}
+
+	fmt.Println(v)
+}
+
+func _29testslice1() {
+	var m = [...]int{1, 2, 3}
+	for i, v := range m {
+		go func() {
+			fmt.Println(i, v)
+		}()
+	}
+
+	time.Sleep(time.Second * 3)
+}
+
+func _29testslice2() {
+	var m = [...]int{1, 2, 3}
+	for i, v := range m {
+		go func(i int, v int) {
+			fmt.Println(i, v)
+		}(i, v)
+	}
+
+	time.Sleep(time.Second * 3)
+}
+
+func _29testslice3() {
+	var m = [...]int{1, 2, 3}
+	for i, v := range m {
+		it := i
+		vt := v
+		go func() {
+			fmt.Println(it, vt)
+		}()
+	}
+
+	time.Sleep(time.Second * 3)
 }
